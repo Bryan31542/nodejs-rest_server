87c1e0576595d4db62bd9517683a65c64a02c3b4
Bryan31542 <00063820@uca.edu.sv>
2022-03-04 22:56:25 -0600

    adding login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,43 @@
 const { response } = require("express");
+const bcrypt = require("bcryptjs");
 
-const login = (req, res = response) => {
-  res.json({
-    msg: "Login OK",
-  });
+const User = require("../models/user");
+
+const login = async (req, res = response) => {
+  const { email, password } = req.body;
+
+  try {
+    // check if email exist
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ msg: "Usuario | Contraseña incorrectas - Email" });
+    }
+
+    // check if users exist
+    if (!user.status) {
+      return res.status(400).json({ msg: "Usuario inactivo" });
+    }
+
+    // check if password is correct
+    const validPassword = bcrypt.compareSync(password, user.password);
+    if (!validPassword) {
+      return res
+        .status(400)
+        .json({ msg: "Usuario | Contraseña incorrectas - Password" });
+    }
+    // generate token
+
+    res.json({
+      msg: "Login OK",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: "Hubo un error, hable con el administrador",
+    });
+  }
 };
 
 module.exports = {

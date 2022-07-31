b5662e4685569b64d137d34bbc77f20d20ed2032
Bryan31542 <00063820@uca.edu.sv>
2022-07-31 00:53:21 -0600

    feat: helper upload

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,34 +1,16 @@
-const path = require("path");
-const { v4: uuidv4 } = require("uuid");
+const { uploadFile } = require("../helpers");
 
-const loadFile = (req, res = response) => {
-  console.log(req.files);
-
+const loadFile = async (req, res = response) => {
   if (!req.files || Object.keys(req.files).length === 0 || !req.files.file) {
     res.status(400).send({ msg: "No files were uploaded." });
     return;
   }
 
-  const { file } = req.files;
-  const nameCut = file.name.split(".");
-  const extension = nameCut[nameCut.length - 1];
+  // Images only
+  const pathComplete = await uploadFile(req.files);
 
-  // Validate extension
-  const validExtensions = ["jpg", "jpeg", "png", "gif"];
-
-  if (!validExtensions.includes(extension)) {
-    return res.status(400).send({ msg: `Invalid file extension ${extension}` });
-  }
-
-  const tempName = `${uuidv4()}.${extension}`;
-  const uploadPath = path.join(__dirname, "../uploads/", tempName);
-
-  file.mv(uploadPath, (err) => {
-    if (err) {
-      return res.status(500).json({ err });
-    }
-
-    res.json({ msg: "File uploaded to " + uploadPath });
+  res.json({
+    path: pathComplete,
   });
 };
 

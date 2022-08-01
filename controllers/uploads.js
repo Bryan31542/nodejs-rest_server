221cf37c351be31e206d82c10b279037b65250d8
Bryan31542 <00063820@uca.edu.sv>
2022-08-01 14:05:50 -0600

    feat: validating empty files

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -3,11 +3,6 @@ const { uploadFile } = require("../helpers");
 const { User, Product } = require("../models");
 
 const loadFile = async (req, res = response) => {
-  if (!req.files || Object.keys(req.files).length === 0 || !req.files.file) {
-    res.status(400).send({ msg: "No files were uploaded." });
-    return;
-  }
-
   try {
     // Images only
     const name = await uploadFile(req.files, undefined, "images");
--- routes/uploads.js
+++ routes/uploads.js
@@ -2,15 +2,16 @@ const { Router } = require("express");
 const { check } = require("express-validator");
 const { loadFile, updateImage } = require("../controllers/uploads");
 const { allowedCollections } = require("../helpers");
-const { validateFields } = require("../middlewares/validate-fields");
+const { validateFields, validateFileToUpload } = require("../middlewares");
 
 const router = Router();
 
-router.post("/", loadFile);
+router.post("/", validateFileToUpload, loadFile);
 
 router.put(
   "/:collection/:id",
   [
+    validateFileToUpload,
     check("id", "Must be a mongo id").isMongoId(),
     check("collection").custom((c) =>
       allowedCollections(c, ["users", "products"])

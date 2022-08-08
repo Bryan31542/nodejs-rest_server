1710f7372dd2db9e9ec68553bd0a72bf89b6bc2b
Bryan31542 <00063820@uca.edu.sv>
2022-08-07 21:05:03 -0600

    feat: validating jwt

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,33 @@
-const socket = io();
+var url = window.location.hostname.includes("localhost")
+  ? "http://localhost:8080/api/auth/"
+  : "https://rest-server2.herokuapp.com/api/auth/";
+
+let user = null;
+let socket = null;
+
+// Validate JWT in localStorage
+const validateJWT = async () => {
+  const token = localStorage.getItem("token") || "";
+
+  if (token.length <= 10) {
+    window.location = "index.html";
+    throw new Error("Token no valido");
+  }
+
+  const res = await fetch(url, {
+    headers: { "x-token": token },
+  });
+
+  const { user: userDB, token: tokenDB } = await res.json();
+
+  localStorage.setItem("token", tokenDB);
+  user = userDB;
+};
+
+const main = async () => {
+  await validateJWT();
+};
+
+main();
+
+//const socket = io();
